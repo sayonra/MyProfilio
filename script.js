6b0d45d0e2e1e94d2bdb9842d1aa38d449a6dc1f
Vet Chhaykey <chhaykeyvet@gmail.com>
2026-04-24 17:39:44 +0700

    Update structure file

--- script.js
+++
@@ -1,4 +0,0 @@
-ScrollReveal().reveal('.hero-text', { delay:200, origin:'left', distance:'50px' });
-ScrollReveal().reveal('.avatar', { delay:300, origin:'right', distance:'50px' });
-ScrollReveal().reveal('.card', { interval:150 });
-ScrollReveal().reveal('.project', { interval:150 });
